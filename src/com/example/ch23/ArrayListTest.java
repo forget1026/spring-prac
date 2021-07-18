e2d2796c42c8e8e5fc6aad4a791f79b74f3e4c59
Hydreamer <dma963@naver.com>
2021-07-19 04:28:40 +0900

    7/19

--- src/com/example/ch23/ArrayListTest.java
+++ src/com/example/ch23/ArrayListTest.java
@@ -1,2 +1,21 @@
-package com.example.ch23;public class ArrayListTest {
+package com.example.ch23;
+
+import com.example.ch21.Book;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Book> library = new ArrayList<>();
+
+        library.add(new Book("태백산맥1", "조정래"));
+        library.add(new Book("태백산맥2", "조정래"));
+        library.add(new Book("태백산맥3", "조정래"));
+        library.add(new Book("태백산맥4", "조정래"));
+        library.add(new Book("태백산맥5", "조정래"));
+
+        for (Book book : library) {
+            book.showInfo();
+        }
+    }
 }
